c088c0cc392659db4a8e8b09cd598b80a6d56930
Michael Mbugua <Michaelngigi76@gmail.com>
2022-02-27 08:17:50 +0300

    working bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {OrderService} from './services/order.service';
+import {AuthService} from './services/auth.service';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
+import {AdminComponent} from './admin/admin.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {NoAccessComponent} from './no-access/no-access.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    AdminComponent,
+    HomeComponent,
+    NotFoundComponent,
+    NoAccessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'admin', component: AdminComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'no-access', component: NoAccessComponent}
+    ])
   ],
-  providers: [],
+  providers: [
+    OrderService,
+
+    AuthService,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
