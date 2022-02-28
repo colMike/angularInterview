eb642821e744a62bf7eedd677024ca40db729e59
Michael Mbugua <Michaelngigi76@gmail.com>
2022-02-28 12:50:35 +0300

    Added functionallities for loan management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
+import {AuthService} from "./services/auth.service";
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,19 @@ import {TranslateService} from "@ngx-translate/core";
 export class AppComponent {
   title = 'Eclectics Web Application';
 
-  constructor(public translate: TranslateService) {
+  constructor(
+    public translate: TranslateService,
+    public authService: AuthService
+  ) {
     translate.addLangs(['en', 'kis']);
     translate.setDefaultLang('kis');
   }
 
   switchLanguage(lang: string){
     this.translate.use(lang);
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 }
